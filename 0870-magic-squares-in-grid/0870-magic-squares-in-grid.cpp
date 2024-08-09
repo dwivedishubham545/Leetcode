4552cbc8b67eaa243b3fc6c3496fe394eef800c4
SHUBHAM DWIVEDI <121412294+dwivedishubham545@users.noreply.github.com>
2024-08-09 22:14:24 +0530

    Time: 6 ms (21.43%), Space: 11.2 MB (51.68%) - LeetHub

--- 0870-magic-squares-in-grid/0870-magic-squares-in-grid.cpp
+++ 0870-magic-squares-in-grid/0870-magic-squares-in-grid.cpp
@@ -1,85 +1,53 @@
 class Solution {
 public:
-    int findRowSum(vector<vector<int>> &grid, int r, int c){
-        vector<bool> visited(10, false);
-        int sum = 0;
-        for(int i=0;i<3;i++){
-            int rowSum=0;
-            for(int j=0;j<3;j++){
-                int val = grid[r+i][c+j];
-                if(val==0 || val>=10 || visited[val]){
-                    return -1;
+    bool isMagicSquare(vector<vector<int>> &grid, int r, int c){
+        vector<bool> visited(10, false);  // To track visited numbers from 1 to 9
+        
+        // Check all values in the 3x3 square
+        for(int i = 0; i < 3; ++i) {
+            for(int j = 0; j < 3; ++j) {
+                int val = grid[r + i][c + j];
+                if (val < 1 || val > 9 || visited[val]) {
+                    return false;
                 }
                 visited[val] = true;
-                rowSum += val;
-            }
-            if(i==0){
-                sum += rowSum;
-            }else{
-                if(sum!=rowSum){
-                    return -1;
-                }
             }
         }
-        return sum;
-    }
-
-    int findColSum(vector<vector<int>> &grid, int r, int c){
-        int sum = 0;
-        for(int j=0;j<3;j++){
-            int colSum=0;
-            for(int i=0;i<3;i++){
-                int val = grid[r+i][c+j];
-                colSum += val;
-            }
-            if(j==0){
-                sum += colSum;
-            }else{
-                if(sum!=colSum){
-                    return -1;
-                }
+        
+        // Check sums of rows, columns, and diagonals
+        int sum = grid[r][c] + grid[r][c+1] + grid[r][c+2]; // First row sum
+        for(int i = 1; i < 3; ++i) {
+            int rowSum = grid[r+i][c] + grid[r+i][c+1] + grid[r+i][c+2];
+            if (rowSum != sum) {
+                return false;
             }
         }
-        return sum;
-    }
-
-    int findDiagonalSum(vector<vector<int>> &grid, int r, int c){
-        int sum1 = grid[r][c] + grid[r+1][c+1] + grid[r+2][c+2];
-        int sum2 = grid[r][c+2] + grid[r+1][c+1] + grid[r+2][c];
-        if(sum1==sum2){
-            return sum1;
+        
+        for(int j = 0; j < 3; ++j) {
+            int colSum = grid[r][c+j] + grid[r+1][c+j] + grid[r+2][c+j];
+            if (colSum != sum) {
+                return false;
+            }
         }
-        return -1;
-    }
-
-    bool isMagicSquare(vector<vector<int>> &grid, int r, int c){
-        int rowSum = findRowSum(grid, r, c);
-        if(rowSum==-1){
+        
+        int diagonalSum1 = grid[r][c] + grid[r+1][c+1] + grid[r+2][c+2];
+        int diagonalSum2 = grid[r][c+2] + grid[r+1][c+1] + grid[r+2][c];
+        
+        if (diagonalSum1 != sum || diagonalSum2 != sum) {
             return false;
         }
-        int colSum = findColSum(grid, r, c);
-        if(colSum==-1){
-            return false;
-        }
-        int diagonalSum = findDiagonalSum(grid, r, c);
-        if(diagonalSum==-1){
-            return false;
-        }
-        if(rowSum==colSum && colSum==diagonalSum){
-            return true;
-        }else{
-            return false;
-        }
+        
+        return true;
     }
 
     int numMagicSquaresInside(vector<vector<int>>& grid) {
         int rows = grid.size();
         int cols = grid[0].size();
-        int cnt=0;
-        for(int i=0;i<=rows-3;i++){
-            for(int j=0;j<=cols-3;j++){
-                if(isMagicSquare(grid, i, j)){
-                    cnt++;
+        int cnt = 0;
+        for(int i = 0; i <= rows - 3; ++i) {
+            for(int j = 0; j <= cols - 3; ++j) {
+                if(isMagicSquare(grid, i, j)) {
+                    ++cnt;
                 }
             }
         }
