71ebea51b69504d447a1d532e06111348321bd3f
SHUBHAM DWIVEDI <121412294+dwivedishubham545@users.noreply.github.com>
2024-10-08 21:55:52 +0530

    Time: 99 ms (77.02%), Space: 32.8 MB (54.72%) - LeetHub

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minSwaps(string s) {
+        int open = 0, close = 0;  
+        for (char ch : s) {
+            if (ch == '[') {
+                open++;  
+            } else {
+                if (open > 0) {
+                    open--;
+                } else {
+                    close++; 
+                }
+            }
+        }
+        return (close + 1) / 2;
+    }
+};
