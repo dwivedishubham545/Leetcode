591f2bec9df2daf7bbabb40c715bcb652eae5fc2
SHUBHAM DWIVEDI <121412294+dwivedishubham545@users.noreply.github.com>
2024-10-10 09:49:38 +0530

    Time: 52 ms (45.61%), Space: 47.7 MB (13.66%) - LeetHub

---
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxWidthRamp(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> stack;
+        int maxWidth = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (stack.empty() || nums[stack.back()] > nums[i]) {
+                stack.push_back(i);
+            }
+        }
+
+        for (int j = n - 1; j >= 0; j--) {
+            while (!stack.empty() && nums[stack.back()] <= nums[j]) {
+                maxWidth = max(maxWidth, j - stack.back());
+                stack.pop_back();
+            }
+        }
+
+        return maxWidth;
+    }
+};
