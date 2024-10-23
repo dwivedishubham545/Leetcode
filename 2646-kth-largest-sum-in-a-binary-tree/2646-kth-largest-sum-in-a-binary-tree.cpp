514e17e7c0690d513006e27d891071d39b2498ab
SHUBHAM DWIVEDI <121412294+dwivedishubham545@users.noreply.github.com>
2024-10-23 10:21:54 +0530

    Time: 13 ms (98.73%), Space: 157.6 MB (5.08%) - LeetHub

---
+++ 2646-kth-largest-sum-in-a-binary-tree/2646-kth-largest-sum-in-a-binary-tree.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long long kthLargestLevelSum(TreeNode* root, int k) {
+        vector<long long>ans; // to store the level sum
+        queue<TreeNode*>q; // for level order traversal
+        q.push(root);
+        while(!q.empty()){
+            long long size = q.size(),sum = 0;
+            for(int i=0;i<size;i++){
+                TreeNode *temp = q.front();
+                q.pop();
+                if(temp->left)q.push(temp->left);
+                if(temp->right)q.push(temp->right);
+                sum += (long long)temp->val;
+            }
+            ans.push_back(sum);
+        }
+        sort(ans.begin(),ans.end()); // sorting the ans vector
+        int n = ans.size();
+        return (n >= k)?ans[n-k] : -1; // returning the result
+    }
+};
