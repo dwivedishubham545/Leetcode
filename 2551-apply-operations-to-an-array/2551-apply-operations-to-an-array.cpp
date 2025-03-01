d2270cab61a16cd21f422593de3ed636c762eb3f
SHUBHAM DWIVEDI <121412294+dwivedishubham545@users.noreply.github.com>
2025-03-01 10:15:33 +0530

    Time: 0 ms (100%), Space: 13.2 MB (62.06%) - LeetHub

---
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1]){
+                nums[i]=2*nums[i];
+                nums[i+1]=0;
+            }
+        }
+        vector<int>ans;
+        int cnt=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                ans.push_back(nums[i]);
+            }else{
+                cnt++;
+            }
+        }
+        for(int i=0;i<cnt;i++){
+            ans.push_back(0);
+        }
+        return ans;
+    }
+};
