0fe80c64583495927d1b464c587b25dfbd34934d
SHUBHAM DWIVEDI <121412294+dwivedishubham545@users.noreply.github.com>
2025-03-05 23:07:11 +0530

    Time: 0 ms (100%), Space: 7.9 MB (31.17%) - LeetHub

---
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    long long coloredCells(int n) {
+        long long ans=1;
+        for(int i=1;i<=n;i++){
+            ans+=(i-1)*4;
+        }
+        return ans;
+    }
+};
