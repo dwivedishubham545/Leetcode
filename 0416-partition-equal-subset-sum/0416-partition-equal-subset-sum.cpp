56ef244cfb0ac8f9a49f7f451b141ef2f2bc6041
SHUBHAM DWIVEDI <121412294+dwivedishubham545@users.noreply.github.com>
2025-04-07 16:40:01 +0530

    Time: 3 ms (99.62%), Space: 14.3 MB (84.17%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
-    bool subsetSumUtil(int ind, int target, vector<int>& arr, vector<vector<int>>& dp) {
-        if (target == 0)
-            return true;
-        if (ind == 0)
-            return arr[0] == target;
-        if (dp[ind][target] != -1)
-            return dp[ind][target];
-
-        bool notTaken = subsetSumUtil(ind - 1, target, arr, dp);
-        bool taken = false;
-        if (arr[ind] <= target)
-            taken = subsetSumUtil(ind - 1, target - arr[ind], arr, dp);
-
-        return dp[ind][target] = notTaken || taken;
-    }
-
     bool canPartition(vector<int>& nums) {
-        int totSum = 0;
-        int n = nums.size();
 
+        int sum = 0, n = nums.size();
         for (int i = 0; i < n; i++) {
-            totSum += nums[i];
+            sum += nums[i];
         }
-
-        if (totSum % 2 == 1)
+        if (sum % 2 != 0)
             return false;
 
-        int k = totSum / 2;
-        vector<vector<int>> dp(n, vector<int>(k + 1, -1));
-        return subsetSumUtil(n - 1, k, nums, dp);
+        bitset<10001> bit(1);
+        for (auto i : nums) {
+            bit |= bit << i;
+            if (bit[sum / 2])
+                return true;
+        }
+        return bit[sum / 2];
     }
 };
